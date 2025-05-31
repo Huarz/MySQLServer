7d9a0a8105d4a1b083dac58a9ad84e5eae39d508
Huarz <13469395926@163.com>
2025-05-31 22:56:42 +0800

    a little name error

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
 {
      if (argc < 3)
     {
-        cerr << "command invalid! example: ./ChatServer 127.0.0.1 6000" << endl;
+        cerr << "command invalid! example: ./SQLServer 127.0.0.1 6000" << endl;
         exit(-1);
     }
 
